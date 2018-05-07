a9572935084cd3f58a2e6f378614b5daa9d74de2
kbpsystem <koleen.bp@outlook.com>
2018-05-08 04:43:04 +0800

    Hide API keys

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
 const twitterApp = {
-    consumer_key: '8rKesm0VBIEZ9NRma61q1Nkub',
-    consumer_secret: 'UtOxtp6ggh7g8tFIrehAIH56b9qvugG2MBkn46MydPI1JoT02a',
-    access_token_key: '731145964816195584-xUiME3bFAxez0WKxicoUvSDAL1ThlTL',
-    access_token_secret: 'zQLBngFtqCPBKIjqSEfl4kVC2We6pWKhJflVLqXU0bBwJ',
-    timeout_ms: 60 * 1000
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
 };
 
 module.exports = {
