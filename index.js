65917d51978e2493d79f771f3a8a073c5e498106
kbpsystem777 <koleen.bp@outlook.com>
2019-03-01 00:01:25 +0800

    adding dotenv and initializing bot framework

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const AutoDM = require('./src/AutoDM.js');
 
-console.log('App Started!');
 
-AutoDM();
+const debug = false;
+const Twit = require('twit');
+require(dotenv).config();
+
+const T = new Twit(require('./config.js'));
+
+//Search parameters
+const hashTag = {
+    q: '', //Enter the keywords to search in Twitter
+    count: 10,
+    result_type: 'recent'
+}
+
+//User Stream
+const stream = T.stream('user');
+stream.on('follow', followed);
+stream.on('tweet', tweetEvent)
+
+//Activities to execute when someone follows this bot
+
+
+
+
+
+
