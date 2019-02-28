c2fb19014a8b7150dd5a44cef3bc558457f7d4b6
kbpsystem777 <koleen.bp@outlook.com>
2019-03-01 01:05:52 +0800

    finishing up the bot

--- config.js
+++ config.js
@@ -1,14 +1,6 @@
-const twitterApp = {
+module.exports = {
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-    timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 };
-
-module.exports = {
-    twitterApp, userName: process.env.USERNAME
-};
-
-
-
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const debug = false;
 const Twit = require('twit');
-require(dotenv).config();
+require('dotenv').config();
 
 const T = new Twit(require('./config.js'));
 
@@ -20,8 +20,46 @@ stream.on('tweet', tweetEvent)
 
 //Activities to execute when someone follows this bot
 
+function followed(event) {
+    let name = event.source.name;
+    let screenName = event.source.screen_name;
+    let response = `Thanks for following me ${name} @${screenName}! Let's support @CIBAC_PartyList`
+    //Posting the thank you tweet:
+    T.post('statuses/update', {status: response}, tweeted);
+
+    //System logging:
+    console.log(`I was followed by ${name}${screenName}`);
+}
+
+function reTweetLatest() {
+    T.get('search/tweets', hashTag, (error, data) => {
+        let tweets = data.status;
+        for(var i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].text)
+        }
+
+        if(!error) { //Run this code if there is no error
+            let reTweetId = data.statuses[0].id_str;
+            //Grab and re-tweet it!
+            T.post('statuses/retweet/' + `${reTweetId}`, {}, tweeted)
+        } else { //If there is an error, run the code below
+            console.log(`There was na error with your search!`, error)
+        }
+    })
+}
+
+function tweeted(err, reply) {
+    if(err !== undefined) {
+        console.log(err)
+    } else {
+        console.log(`Tweeted: ` + reply)
+    }
+}
 
 
+//Re-tweet something as soon as the program runs:
+reTweetLatest()
 
 
-
+//Re-tweet something after every minute
+setInterval(reTweetLatest(), 1000 * 60 * 12);
